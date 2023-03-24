a627d847e08101c00ef9990f8947713d996f7c60
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-24 11:49:59 +0700

    Graph_with_point

--- simpl_plot.py
+++ simpl_plot.py
@@ -5,12 +5,22 @@ class Simple_graph():
 
     def __init__(self) -> None:
         
+        self.fig = plt.figure()
+
         return None
 
-    def __add_point_on_g__(self) -> None:
+    def __add_point_on_g__(self, x, y) -> None:
+        
+
+        scatter1 = plt.scatter(0.0, 1.0)
+        #print('Scatter: ', type(scatter1))
+        grid1 = plt.grid(True) # линии вспомогательной сетки
+        plt.show()
 
         return None
 
     def draw(self):
+
+        self.__add_point_on_g__(10, 10)
 
         return None
