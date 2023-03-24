1c4ccf2f7131555b774eb4bde533d1f957843605
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-24 11:36:48 +0700

    Work_with_first_grah_class

---
+++ simpl_plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Simple_graph():
+
+    def __init__(self) -> None:
+        
+        return None
+
+    def __add_point_on_g__(self) -> None:
+
+        return None
+
+    def draw(self):
+
+        return None
